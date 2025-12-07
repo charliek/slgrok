b06f34116fe0cfed10cae872365ed070d39a2e7c
charliek <charlie.knudsen@gmail.com>
2025-12-07 17:58:22 -0600

    Update to not fail with .env files

--- src/slgrok/settings.py
+++ src/slgrok/settings.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
         env_prefix="SLGROK_",
         env_file=".env",
         env_file_encoding="utf-8",
+        extra="ignore",
     )
 
     base_url: HttpUrl = HttpUrl("http://127.0.0.1:4040")
